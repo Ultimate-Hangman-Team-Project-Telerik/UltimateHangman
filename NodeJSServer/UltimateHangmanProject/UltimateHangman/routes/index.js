61bcd785fbab385328d76a02bce6f03b2dee1da8
theshaftman <mariyan_apostolov@mail.bg>
2017-05-02 01:59:54 +0300

    Ejs modification applied

--- NodeJSServer/UltimateHangmanProject/UltimateHangman/routes/index.js
+++ NodeJSServer/UltimateHangmanProject/UltimateHangman/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('home/home', { title: 'Ultimate Hangman', people: [{ name: "Petar" }, { name: "Ivan" }] });
+    res.render('index', { title: 'Ultimate Hangman', people: [{ name: "Petar" }, { name: "Ivan" }] });
 });
 
 router.get('/login', function (req, res) {
