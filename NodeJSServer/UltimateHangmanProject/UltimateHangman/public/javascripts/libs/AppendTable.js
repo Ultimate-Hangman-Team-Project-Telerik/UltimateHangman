c40cc99145631731e1b96a0f12d58c8597b2db96
theshaftman <mariyan_apostolov@mail.bg>
2017-05-07 17:57:28 +0300

    Append table plugin

---
+++ NodeJSServer/UltimateHangmanProject/UltimateHangman/public/javascripts/libs/AppendTable.js
@@ -0,0 +1,20 @@
+﻿var AppendTable = (function () {
+    init = function (data) {
+        // table appending :)
+    }
+    
+    return {
+        init: init
+    }
+}());
+
+
+//$.ajax({
+//    type: "GET",
+//    url: "/...",
+//    data: {
+//        page: 1
+//    }
+//}).done(function (response) { 
+//    AppendTable.init(response);
+//});
